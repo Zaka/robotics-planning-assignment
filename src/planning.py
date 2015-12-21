43575889622b0a0c80f5d15d2ee9efef57a1d871
Zaka Elab <zakaelab@openmailbox.org>
2015-12-21 20:35:47 +0100

    Add event handling
    1 - Added mouse clicking for start and end definition.

--- src/planning.py
+++ src/planning.py
@@ -6,58 +6,65 @@ import numpy as np
 from PIL import Image, ImageTk
 
 # Read image
-class mainWindow():
-        # times=1
-        # timestart=time.clock()
+class MainWindow():
+    def __init__(self):
+        self.data = mpimg.imread('../maze-images/laberinto-01.png')
+        self.data = (self.data * 255).round().astype(np.uint8)
 
-        data = mpimg.imread('../maze-images/laberinto-01.png')
-
-        data = (data * 255).round().astype(np.uint8)
-
-        width = data.shape[1]
-        height = data.shape[0]
-
-
-        # data=np.array(np.random.random((400,500))*100,
-        #                  dtype=int)
+        # TODO: Transform the data to a 0, inf matrix. Where inf is
+        # obstacle.
         
-        def __init__(self):
-            self.root = Tkinter.Tk()
-            self.frame = Tkinter.Frame(self.root,
-                                       width=self.width,
-                                       height=self.height)
-            self.frame.pack()
-            self.canvas = Tkinter.Canvas(self.frame,
-                                         width=self.width,
-                                         height=self.width)
-            # self.canvas.place(x=-2,y=-2)
-            self.canvas.place(x=0,y=0)
-            self.root.after(0,self.start) # INCREASE THE 0 TO SLOW IT
-                                          # DOWN
+        self.width = self.data.shape[1]
+        self.height = self.data.shape[0]
 
-            print "width", self.width
-            print "height", self.height
+        self.root = Tkinter.Tk()
+        self.frame = Tkinter.Frame(self.root,
+                                   width=self.width,
+                                   height=self.height)
+        self.frame.pack()
+        self.canvas = Tkinter.Canvas(self.frame,
+                                     width=self.width,
+                                     height=self.width)
+        # self.canvas.place(x=-2,y=-2)
+        self.canvas.place(x=0,y=0)
 
-            self.root.mainloop()
+        # Mouse event handling
+        self.root.bind('<Button-1>', self.onLeftButton)
+        self.root.bind('<Button-3>', self.onRightButton)
+        
+        self.root.after(0,self.start)
+        self.root.mainloop()
                 
-        def start(self):
-            global data
-            self.im=Image.fromstring('L', (self.data.shape[1],\
-                                           self.data.shape[0]),
-                                     self.data.astype('b').tostring())
-            self.photo = ImageTk.PhotoImage(image=self.im)
-            self.canvas.create_image(0,0,
-                                     image=self.photo,
-                                     anchor=Tkinter.NW)
-            self.root.update()
-            # self.times+=1
-            # if self.times%33==0:
-            #     print "%.02f FPS"%(self.times/(time.clock()-self.timestart))
-            # self.root.after(10,self.start)
-            # self.data=np.roll(self.data,-1,1)
+    def start(self):
+        self.im=Image.fromstring('L', (self.data.shape[1],\
+                                       self.data.shape[0]),
+                                 self.data.astype('b').tostring())
+        self.photo = ImageTk.PhotoImage(image=self.im)
+        self.canvas.create_image(0,0,
+                                 image=self.photo,
+                                 anchor=Tkinter.NW)
+        self.root.update()
 
-if __name__ == '__main__':
-    x=mainWindow()
+    def onLeftButton(self, event):
+        # TODO: Mark as start of the path
+        self.canvas.create_line(event.x, event.y,
+                                event.x + 1, event.y)
+        print "Left button on", event.x, event.y
+        # TODO: Fire the createPath method only if start and end are
+        # defined
+
+    def onRightButton(self, event):
+        # TODO: Mark as the end of the path
+        self.canvas.create_line(event.x, event.y,
+                                event.x + 1, event.y)
+        print "Right button on", event.x, event.y
+        # TODO: Fire the createPath method only if start and end are
+        # defined
+
+        
+# TODO: Uncomment and indent        
+# if __name__ == '__main__':
+x=MainWindow()
 
 # TODO: Show image in a interactive window
 
