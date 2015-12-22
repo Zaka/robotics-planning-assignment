2e8527ef810ec256a89531b62c33e8a1a2bf5e3f
Zaka Elab <zakaelab@gmail.com>
2015-12-22 18:30:28 +0100

    Paint image before scale
    1 - Add the capability to paint the image before scaling it, so it is
    not possible to paint a surface less than the represented by the maze
    matrix.
    2 - Establish init window size
    3 - Get rid of matplot lib
    3 - Get map from a PIL image

--- src/planning.py
+++ src/planning.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import pdb
 import Tkinter as tk
 import matplotlib.image as mpimg
 import numpy as np
@@ -8,30 +9,33 @@ from PIL import Image, ImageTk
 # Read image
 class MainWindow():
     def __init__(self):
-        self.data = mpimg.imread('../maze-images/maze-01.png')
-        self.data = (self.data * 255).round().astype(np.uint8)
-
-        # TODO: Transform the data to a 0, inf matrix. Where inf is
-        # obstacle.
-        self.maze = self.getMap(self.data)
-        
-        self.width = self.data.shape[1]
-        self.height = self.data.shape[0]
-
         self.root = tk.Tk()
+        
+        self.im = Image.open('../maze-images/maze-01.png')
+
+        self.photo = ImageTk.PhotoImage(image = self.im)
+        
+        self.imgSize = { 'width' : self.photo.width(),
+                         'height' : self.photo.height() }
+        
+        self.windowSize = dict(self.imgSize)
+        
+        self.maze = self.getMap(self.im.getdata())
+
+        initSize = { 'w' : 400, 'h' : 400 }
+        
         self.frame = tk.Frame(self.root,
-                                   width=self.width,
-                                   height=self.height)
+                              width = initSize['w'],
+                              height = initSize['h'])
         self.frame.columnconfigure(0, weight = 1)
         self.frame.rowconfigure(0, weight = 1)
         self.frame.pack(fill = BOTH, expand = 1)
         self.canvas = tk.Canvas(self.frame,
                                 bd = 0,
                                 highlightthickness = 0,
-                                width=self.width,
-                                height=self.width)
+                                width=initSize['w'],
+                                height=initSize['h'])
         self.canvas.grid(row = 0, sticky = W+E+N+S)
-        # self.canvas.place(x=-2,y=-2)
         self.canvas.place(x=0,y=0)
 
         # Mouse event handling
@@ -39,55 +43,78 @@ class MainWindow():
         self.root.bind('<Button-3>', self.onRightButton)
         # Resize event handling
         self.frame.bind('<Configure>', self.onResize)
-        
-        self.root.after(0,self.start)
+
+        self.imageOnCanvas = self.canvas.create_image(0,0,
+                                 image = self.photo,
+                                 anchor=tk.NW)
+    
         self.root.mainloop()
                 
-    def start(self):
-        self.im=Image.fromstring('L', (self.data.shape[1],\
-                                       self.data.shape[0]),
-                                 self.data.astype('b').tostring())
-        self.photo = ImageTk.PhotoImage(image=self.im)
-        self.canvas.create_image(0,0,
-                                 image=self.photo,
-                                 anchor=tk.NW)
-        self.root.update()
-
     def onLeftButton(self, event):
         # TODO: Mark as start of the path
-        self.canvas.create_line(event.x, event.y,
-                                event.x + 1, event.y,
-                                fill='red')
-        print "Left button on", event.x, event.y
+        
+        x = self.imgSize['width']*event.x / self.windowSize['width']
+        y = self.imgSize['height']*event.y / self.windowSize['height']
+
+        self.im.putpixel((x,y), 230)
+
+        resized = self.im.resize((self.windowSize['width'],
+                                  self.windowSize['height']),
+                                 Image.ANTIALIAS)
+        
+        self.photo = ImageTk.PhotoImage(image = resized)
+        
+        self.canvas.config(width = self.windowSize['width'],
+                           height = self.windowSize['height'])
+        self.canvas.itemconfig(self.imageOnCanvas,
+                               image = self.photo)
+        self.root.update()
         # TODO: Fire the createPath method only if start and end are
         # defined
 
     def onRightButton(self, event):
         # TODO: Mark as the end of the path
-        self.canvas.create_line(event.x, event.y,
-                                event.x + 1, event.y,
-                                fill='green')
-        print "Right button on", event.x, event.y
+        x = self.imgSize['width']*event.x / self.windowSize['width']
+        y = self.imgSize['height']*event.y / self.windowSize['height']
+
+        self.im.putpixel((x,y), 150)
+
+        resized = self.im.resize((self.windowSize['width'],
+                                  self.windowSize['height']),
+                                 Image.ANTIALIAS)
+        
+        self.photo = ImageTk.PhotoImage(image = resized)
+        
+        self.canvas.config(width = self.windowSize['width'],
+                           height = self.windowSize['height'])
+        self.canvas.itemconfig(self.imageOnCanvas,
+                               image = self.photo)
+        self.root.update()
+        
         # TODO: Fire the createPath method only if start and end are
         # defined
 
     def onResize(self, event):
         size = (event.width, event.height)
+        
+        self.windowSize['width'] = event.width
+        self.windowSize['height'] = event.height
+        
         resized = self.im.resize(size,Image.ANTIALIAS)
-        self.image = ImageTk.PhotoImage(resized)
+        self.photo = ImageTk.PhotoImage(resized)
         self.canvas.config(width = event.width,
                            height = event.height)
         self.canvas.delete("IMG")
-        self.canvas.create_image(0, 0, image=self.image,
+        self.canvas.create_image(0, 0, image=self.photo,
                                   anchor=tk.NW, tags="IMG")
         self.root.update()
 
     def getMap(self, data):
-        maze = np.full(shape = data.shape,
+        maze = np.full(shape = data.size,
                        fill_value = float('inf'))
 
-        for index, value in np.ndenumerate(self.data):
-            if value == 255:
+        for i in np.ndindex(data.size):
+            if data.getpixel(i) == 255:
                 maze[index] = 0
 
         return maze
